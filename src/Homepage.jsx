5820798120ffd83ec4f58c2ffcd83bde9e75ff56
Nolan Reilly <nolanreilly02@gmail.com>
2025-03-21 14:17:57 -0500

    Create Suggested Content Component

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Post from "./Post";
+import SuggestedContent from "./SuggestContent";
 
 export default function Homepage() {
   return (
@@ -26,49 +27,7 @@ export default function Homepage() {
             </div>
           </div>
 
-          <div className="suggested-content gap-8">
-            <p className="weight-600 text-small">Suggested for you</p>
-
-            <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
-              <div className="large-circle-icon width-24"></div>
-              <div className="flex flex-col">
-                <p className="text-medium">Username</p>
-                <p className="text-small">Lorem ipsum dolor sit.</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
-              <div className="large-circle-icon width-24"></div>
-              <div className="flex flex-col">
-                <p className="text-medium">Username</p>
-                <p className="text-small">Lorem ipsum dolor sit.</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
-              <div className="large-circle-icon width-24"></div>
-              <div className="flex flex-col">
-                <p className="text-medium">Username</p>
-                <p className="text-small">Lorem ipsum dolor sit.</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
-              <div className="large-circle-icon width-24"></div>
-              <div className="flex flex-col">
-                <p className="text-medium">Username</p>
-                <p className="text-small">Lorem ipsum dolor sit.</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
-              <div className="large-circle-icon width-24"></div>
-              <div className="flex flex-col">
-                <p className="text-medium">Username</p>
-                <p className="text-small">Lorem ipsum dolor sit.</p>
-              </div>
-            </div>
-          </div>
+          <SuggestedContent />
         </div>
       </div>
     </div>
---
+++ src/SuggestContent.jsx
@@ -0,0 +1,47 @@
+export default function SuggestedContent() {
+  return (
+    <div className="suggested-content gap-8">
+      <p className="weight-600 text-small">Suggested for you</p>
+
+      <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
+        <div className="large-circle-icon width-24"></div>
+        <div className="flex flex-col">
+          <p className="text-medium">Username</p>
+          <p className="text-small">Lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
+        <div className="large-circle-icon width-24"></div>
+        <div className="flex flex-col">
+          <p className="text-medium">Username</p>
+          <p className="text-small">Lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
+        <div className="large-circle-icon width-24"></div>
+        <div className="flex flex-col">
+          <p className="text-medium">Username</p>
+          <p className="text-small">Lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
+        <div className="large-circle-icon width-24"></div>
+        <div className="flex flex-col">
+          <p className="text-medium">Username</p>
+          <p className="text-small">Lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row p-8 border-radius gap-16 bg-dark-gray">
+        <div className="large-circle-icon width-24"></div>
+        <div className="flex flex-col">
+          <p className="text-medium">Username</p>
+          <p className="text-small">Lorem ipsum dolor sit.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
