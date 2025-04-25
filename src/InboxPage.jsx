351b3d49d90bf6e01cd6fdca219f6100fefa0b9b
fascination-street <pedromagnanimo@proton.me>
2025-04-25 11:28:54 -0500

    Inbox page

--- src/InboxPage.jsx
+++ src/InboxPage.jsx
@@ -1,11 +1,100 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import InboxUser from "./components/InboxUser.jsx";
 
 export default function InboxPage() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <div>
-      <p>Inbox Page</p>
+      {/* Navbar for mobile messaging */}
+      {/*I just copied what Sean had here*/}
+      <div className="navbar bg-light-black">
+        <nav className="container flex flex-row justify-between align-center py-24">
+          <Link to="/inbox">
+            <div className="position-relative">
+              <img
+                className="width-32 svg-white"
+                src="./backarrow.svg"
+                alt="Back"
+              />
+            </div>
 
-      <Link to="/messaging">Message a User</Link>
+          </Link>
+          <div className="flex flex-row align-center gap-8">
+            <p className="text-small weight-700">User</p>
+          </div>
+
+          <img
+            className="width-32 svg-white"
+            src="./plusnobox.svg"
+            alt="Send Post"
+            onClick={togglePopup}
+          />
+        </nav>
+      </div>
+
+      <div className="bg-light-gray flex flex-col align-center gap-16 padding-16">
+        <p>Search bar here</p>
+      </div>
+
+      <div className="bg-dark-black flex flex-col align-center margin-8 gap-8 padding-48">
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+        <InboxUser lastmsg="wow that's awesome!"/>
+      </div>
+
+      {/* Popup for choosing a person to message*/}
+      {showPopup && (
+        <div className="popup-overlay flex flex-row" onClick={togglePopup}>
+          <div className="popup-menu">
+            <p className="text-medium weight-700">Choose a person to message</p>
+            <ul className="flex flex-row gap-128 margin-8 padding-16 align-center justify-center" style={{listStyle: 'none'}}>
+              {/*These could (and should) be turned into components I just didn't*/}
+                <li>
+                  <Link to="/messaging" className="text-no-underline">
+                  <img
+                    className="width-64 rounded-full margin-16 large-circle-icon" 
+                    src="public/profile-picture-01.jpg"/>
+                    <p className="text-small weight-700">User</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/messaging" className="text-no-underline">
+                  <img
+                      className="width-64 rounded-full margin-16 large-circle-icon" 
+                      src="public/profile-picture-04.jpg"/>
+                      <p className="text-small weight-700">User</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/messaging" className="text-no-underline">
+                  <img
+                      className="width-64 rounded-full margin-16 large-circle-icon" 
+                      src="public/profile-picture-02.jpg"/>
+                      <p className="text-small weight-700">User</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/messaging" className="text-no-underline">
+                  <img
+                      className="width-64 rounded-full margin-16 large-circle-icon" 
+                      src="public/profile-picture-03.jpg"/>
+                      <p className="text-small weight-700">User</p>
+                  </Link>
+                </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/InboxUser.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function InboxUser({lastmsg}) {
+    return (
+        <div className="align-center full-width margin-16 rounded-box">
+            <Link to="/messaging" className="full-width text-no-underline">
+            <div className="bg-medium-black gap-8 flex flex-row align-center full-width">
+                {/*User profile picture*/}
+                <img
+                    className="width-48 rounded-full margin-16 large-circle-icon"
+                    src="public/profile-picture-02.jpg"
+                    alt="User"
+                />
+
+                {/*User name and last message*/}
+                <div className="flex flex-col">
+                    <p className="text-small weight-700">User</p>
+                    <p className="text-small weight-400">{lastmsg}</p>
+                </div>
+
+                {/*Camera icon, moved all the way to the right end*/}
+                <div className="flex flex-row align-flex-end gap-8 p-8 margin-16" style={{marginLeft: 'auto'}}>
+                    <img
+                        className="width-32 svg-white"
+                        src="./camera.svg"
+                        alt="Take Picture"
+                    />
+                </div>
+            </div>
+            </Link>
+        </div> 
+    );
+}
